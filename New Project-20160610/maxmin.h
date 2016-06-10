830cd938ce32f74ae2f86ce3d0c0d157a632cf42
wxyyoyo <wxy.wang2013@gmail.com>
2016-06-10 15:50:13 -0500

    Created by Coding Ground Client

---
+++ New Project-20160610/maxmin.h
@@ -0,0 +1,17 @@
+#ifndef MAXMIN_H
+#define MAXMIN_H
+
+#include<cmath>
+
+inline double max(double a, double b)
+{
+    return a>b? a:b;
+}
+
+inline double min(double a, double b)
+{
+    return a<b? a:b;
+}
+
+
+#endif
